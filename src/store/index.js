14c9e0012476bb7c63e3b6b9edd5cfad96d1af81
katerynaprokofieva <gucolk@gmail.com>
2021-01-31 19:05:29 +0200

    adding redux dev tool extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import BirdsService from '../services';
 import { reducer } from './reducer';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 const birdsService = new BirdsService();
 
 export default store;
